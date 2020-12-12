60c428822367a45cf81c58c162c9d846bc8da912
wanyaworld <6812skiii@gmail.com>
2020-12-13 01:20:23 +0900

    Init

---
+++ custom_class_as_a_unordered_map_key/main.cc
@@ -0,0 +1,52 @@
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+struct Key
+{
+  std::string first;
+  std::string second;
+  int         third;
+
+  bool operator==(const Key &other) const
+  { return (first == other.first
+            && second == other.second
+            && third == other.third);
+  }
+};
+
+namespace std {
+
+  template <>
+  struct hash<Key>
+  {
+    std::size_t operator()(const Key& k) const
+    {
+      using std::size_t;
+      using std::hash;
+      using std::string;
+
+      // Compute individual hash values for first,
+      // second and third and combine them using XOR
+      // and bit shifting:
+
+      return ((hash<string>()(k.first)
+               ^ (hash<string>()(k.second) << 1)) >> 1)
+               ^ (hash<int>()(k.third) << 1);
+    }
+  };
+
+}
+
+int main() {
+	struct Key key;
+	key.first = "f";
+	key.second = "s";
+	key.third = 3;
+
+	unordered_map<struct Key, bool> mymap;
+	mymap[key] = true;
+
+	return 0;
+}
