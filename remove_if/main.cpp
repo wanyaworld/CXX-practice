0ff36dd7fdadfff649071c08e144303091cf2339
Jangwoong Kim <jangwoong.kim@moreh.io>
2021-04-01 11:51:11 +0900

    add

--- remove_if/main.cpp
+++ remove_if/main.cpp
@@ -1,12 +1,22 @@
 #include <vector>
 #include <algorithm>
 
+bool isOdd(int x) {
+  return x % 2;
+}
+
 void removeOdds(std::vector<int> &v) {
   v.erase(std::remove_if(v.begin(), v.end(), [](int i) { return i % 2; }), v.end());
+}
+
+void removeOddsFunc(std::vector<int> &v) {
+  v.erase(std::remove_if(v.begin(), v.end(), isOdd), v.end());
 }
 
 int main (){
   std::vector<int> v {1, 4, 3, 2, 5};
   removeOdds(v);
+  std::vector<int> v2 {1, 4, 3, 2, 5};
+  removeOddsFunc(v2);
   return 0;
 }
