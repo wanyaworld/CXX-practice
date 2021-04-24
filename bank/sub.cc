97b0be14750d1432c1414ba794c7e580f4374786
root <root@DESKTOP-GN23183.localdomain>
2021-04-24 15:50:49 +0900

    asd

--- bank/sub.cc
+++ bank/sub.cc
@@ -15,7 +15,7 @@ struct node {
   struct node *next;
 };
 
-// atm 기기를 나타내는 녿,
+// atm 기기를 나타내는 노드
 struct atm {
   struct node *head, *tail;
   int people_waiting;
@@ -24,7 +24,58 @@ struct atm {
 struct atm *patms[3];
 
 // 큐에서 삭제하는 함수
-void del(struct atm *patm) {
+void dequeue(struct atm *patm);
+
+// 큐에 삽입하는 함수
+void insertNode (struct node *pnode, int n) ;
+
+
+// 가장 적은 대기줄을 갖는 atm에 사람을 삽입하는 함수
+void insertToAtm (struct node* pnode, int epoch) ;
+
+
+// 매 분마다 호출되어 총 기다린 시간을 증가시키고
+// 현재 업무를 보는 사람의 소요시간을 감소시켜
+// 업무를 다 보았으면 큐에서 제거하는 함수
+void execute() ;
+
+
+// 총 기다리는 사람 수를 리턴하는 함수
+int numWaiting() ;
+
+
+// 메인함수
+int main() {
+  srand(time(NULL));
+  for (int i = 0 ; i < 3 ; i++)
+    patms[i] = (struct atm*)malloc(sizeof(struct atm*) * 3);
+  while (1) {
+    cur_time++;
+    if (cur_time == 120) break;
+    struct node *pnode;
+    pnode = (struct node*)malloc(sizeof(struct node));
+    pnode->remaining_min = rand() % 9 + 2;
+    pnode->start_min = cur_time;
+    insertToAtm(pnode, cur_time);
+    execute();
+  }
+
+  printf("총 %d 명의 사람이 업무를 보았습니다.\n", finished_people_num);
+  printf("평균 %f 분을 기다렸습니다. \n", (float)waited_min / people_num);
+  printf("총 %d 명이 기다리고 있습니다. \n", numWaiting());
+  
+  for (int i = 0 ; i < 3 ; i++) {
+    struct node* next;
+    for (struct node *p = patms[i]->head ; p ; p = next) {
+      next = p->next;
+      free (p);
+    }
+    free(patms[i]);
+  }
+  return 0;
+}
+
+void dequeue(struct atm *patm) {
   if (!patm->head) return;
   struct node* prv_head = patm->head;
   if (patm->head != patm->tail) {
@@ -34,12 +85,12 @@ void del(struct atm *patm) {
     patm->head = patm->tail = NULL;
   }
   free (prv_head);
-  if (patm->people_waiting >= 1)
-    (patm->people_waiting)--;
+  (patm->people_waiting)--;
 }
 
 // 큐에 삽입하는 함수
-void insert(struct atm *patm, struct node *pnode) {
+void insertNode (struct node *pnode, int n) {
+  struct atm *patm = patms[n];
   if (!patm->tail) {
     patm->tail = pnode;
     patm->head = pnode;
@@ -49,10 +100,6 @@ void insert(struct atm *patm, struct node *pnode) {
     patm->tail = pnode;
   }
   (patm->people_waiting)++;
-}
-
-void insertNode (struct node *pnode, int n) {
-  insert(patms[n], pnode);
 }
 
 // 가장 적은 대기줄을 갖는 atm에 사람을 삽입하는 함수
@@ -83,10 +130,12 @@ void execute() {
       waited_min += waiting_people_num;
     }
     if (cur_atm->head) {
-      if ((cur_atm->head->remaining_min)-- <= 0) {
-	del(cur_atm);
+      if (cur_atm->head->remaining_min <= 0) {
+	dequeue(cur_atm);
 	finished_people_num++;
       }
+      else
+        (cur_atm->head->remaining_min) -= 1;
     }
   }
 }
@@ -103,35 +152,4 @@ int numWaiting() {
   return ret;
 }
 
-
-// 메인함수
-int main() {
-  srand(time(NULL));
-  for (int i = 0 ; i < 3 ; i++)
-    patms[i] = (struct atm*)malloc(sizeof(struct atm*) * 3);
-  while (1) {
-    cur_time++;
-    if (cur_time == 120) break;
-    struct node *pnode;
-    pnode = (struct node*)malloc(sizeof(struct node));
-    pnode->remaining_min = rand() % 9 + 2;
-    pnode->start_min = cur_time;
-    insertToAtm(pnode, cur_time);
-    execute();
-  }
-
-  printf("총 %d 명의 사람이 업무를 보았습니다.\n", finished_people_num);
-  printf("평균 %f 분을 기다렸습니다. \n", (float)waited_min / people_num);
-  printf("총 %d 명이 기다리고 있습니다. \n", numWaiting());
-  
-  for (int i = 0 ; i < 3 ; i++) {
-    struct node* n;
-    for (struct node *p = patms[i]->head ; p ; p = n) {
-      n = p->next;
-      free (p);
-    }
-    free(patms[i]);
-  }
-  return 0;
-}
 
