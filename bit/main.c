136a1dff590417b8637767b080ab5041cdd30d70
JangwoongKim <6812skiii@gmail.com>
2021-04-26 15:20:50 +0900

    asd

--- bit/main.c
+++ bit/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int isOddTen(int n) {
+	return (n / 10) % 2;
+}
+
+int isOddThou(int n) {
+	return (n / 1000) % 2;
+}
+
 int downOdd(int n) {
-	return n & 0xaaaaaaaa;
+	return n & 0x55555555;
 }
 int upEven(int n) {
 	return n | 0xaaaaaaaa;
@@ -9,21 +17,33 @@ int upEven(int n) {
 int flipEven(int n) {
 	int op1 = n & 0x55555555;
 	int op2 = n & 0xaaaaaaaa;
-	op2 = ~op2;
+	op1 |= 0xaaaaaaaa;
+	op1 = ~op1;
 	return op1 | op2;
 }
-int shiftRight(int n) {
-	return n >> 1;
+int shiftRight(int n, int shift) {
+	return n >> shift;
 }
 
 int main() {
 	int num = 20061234;
 	for (int i = 0 ; i < 4 ; i++) {
 		num += 510 * i;
-		printf("%d\n", downOdd(num));
-		printf("%d\n", upEven(num));
-		printf("%d\n", flipEven(num));
-		printf("%d\n", shiftRight(num));
+		int print;
+		char c;
+		if (isOddTen(num) && isOddThou(num)) {
+			c = 'A';
+			print = downOdd(num); }
+		else if (!isOddTen(num) && isOddThou(num))  {
+			c = 'B';
+			print = upEven(num); }
+		else if (isOddTen(num) && !isOddThou(num)) {
+			c = 'C';
+			print = flipEven(num); }
+		else if (!isOddTen(num) && !isOddThou(num)) {
+			c = 'D';
+			print = shiftRight(num, num % 10); }
+		printf("For case %c, the result is %d\n", c, print);
 	}
 	return 0;
 }
