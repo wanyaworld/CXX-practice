488dd7370619bd9204f3fc57a7baf016e116e471
JangwoongKim <6812skiii@gmail.com>
2021-04-30 13:25:30 +0900

    ASd

--- placement-new/main.cc
+++ placement-new/main.cc
@@ -10,9 +10,17 @@ public:
 int main() {
 	char *buf = new char[0x1000];
 	MyClass *pmc = new MyClass();
+
+	/* placement-new upon buffer */
 	MyClass *pmc_place = new (buf) MyClass();
+
+	/* delete buffer first */
 	delete [] buf;
-	delete pmc;
+	
+	/* then manually call destructor */
 	pmc_place->~MyClass();
+	
+	delete pmc;
+	
 	return 0;
 }
